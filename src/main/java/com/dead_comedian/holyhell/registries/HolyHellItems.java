ff693687737481637fbb9abb0383bad4f389bf16
The-Dead-Comedian <113227369+The-Dead-Comedian@users.noreply.github.com>
2024-09-09 21:28:58 +0300

    Fixed Jsons with Codenameds display preset and added lang entries

--- src/main/java/com/dead_comedian/holyhell/registries/HolyHellItems.java
+++ src/main/java/com/dead_comedian/holyhell/registries/HolyHellItems.java
@@ -16,22 +16,24 @@ import net.minecraft.util.Identifier;
 
 public class HolyHellItems {
     public static final Item SAINT_EYE = registerItem("saint_eye", new SaintEyeItem(new FabricItemSettings().food(HolyHellFoodComponents.SAINT_EYE)));
-    public static final Item HOLY_GRAIL= registerItem("holy_grail", new HolyGrailItem(HolyHellEntities.CHRISTIANCROSS, new FabricItemSettings()));
-    public static final Item RELIGIOUS_RINGS= registerItem("religious_rings", new ReligiousRingsItem(new FabricItemSettings()));
-    public static final Item ANGEL_SPAWN_EGG = registerItem("angel_spawn_egg", new SpawnEggItem(HolyHellEntities.ANGEL,0xffefbf,0xd19822, new FabricItemSettings()));
-    public static final Item KAMIKAZE_ANGEL_SPAWN_EGG = registerItem("kamikaze_angel_spawn_egg", new SpawnEggItem(HolyHellEntities.KAMIKAZE_ANGEL,0xffefbf,0xd19822, new FabricItemSettings()));
-    public static final Item HERETIC_SPAWN_EGG = registerItem("heretic_spawn_egg", new SpawnEggItem(HolyHellEntities.HAILING_HERETIC,0xffefbf,0xf5c842, new FabricItemSettings()));
+    public static final Item HOLY_GRAIL = registerItem("holy_grail", new HolyGrailItem(HolyHellEntities.CHRISTIANCROSS, new FabricItemSettings()));
+    public static final Item RELIGIOUS_RINGS = registerItem("religious_rings", new ReligiousRingsItem(new FabricItemSettings()));
+    public static final Item ANGEL_SPAWN_EGG = registerItem("angel_spawn_egg", new SpawnEggItem(HolyHellEntities.ANGEL, 0xffefbf, 0xd19822, new FabricItemSettings()));
+    public static final Item KAMIKAZE_ANGEL_SPAWN_EGG = registerItem("kamikaze_angel_spawn_egg", new SpawnEggItem(HolyHellEntities.KAMIKAZE_ANGEL, 0xffefbf, 0xd19822, new FabricItemSettings()));
+    public static final Item HERETIC_SPAWN_EGG = registerItem("heretic_spawn_egg", new SpawnEggItem(HolyHellEntities.HAILING_HERETIC, 0xffefbf, 0xf5c842, new FabricItemSettings()));
     public static final Item HOLY_TEAR = registerItem("holy_tear", new Item(new FabricItemSettings()));
-    public static final Item EVANGELIST_HELMET= registerItem("evangelist_helmet", new ArmorItem(ModArmorMaterials.EVANGELIST, ArmorItem.Type.HELMET,new FabricItemSettings()));
-    public static final Item EVANGELIST_CHESTPLATE= registerItem("evangelist_chestplate", new ArmorItem(ModArmorMaterials.EVANGELIST, ArmorItem.Type.CHESTPLATE,new FabricItemSettings()));
-    public static final Item EVANGELIST_LEGGINGS= registerItem("evangelist_leggings", new ArmorItem(ModArmorMaterials.EVANGELIST, ArmorItem.Type.LEGGINGS,new FabricItemSettings()));
-    public static final Item EVANGELIST_BOOTS= registerItem("evangelist_boots", new ArmorItem(ModArmorMaterials.EVANGELIST, ArmorItem.Type.BOOTS,new FabricItemSettings()));;;
+    public static final Item EVANGELIST_HELMET = registerItem("evangelist_helmet", new ArmorItem(ModArmorMaterials.EVANGELIST, ArmorItem.Type.HELMET, new FabricItemSettings()));
+    public static final Item EVANGELIST_CHESTPLATE = registerItem("evangelist_chestplate", new ArmorItem(ModArmorMaterials.EVANGELIST, ArmorItem.Type.CHESTPLATE, new FabricItemSettings()));
+    public static final Item EVANGELIST_LEGGINGS = registerItem("evangelist_leggings", new ArmorItem(ModArmorMaterials.EVANGELIST, ArmorItem.Type.LEGGINGS, new FabricItemSettings()));
+    public static final Item EVANGELIST_BOOTS = registerItem("evangelist_boots", new ArmorItem(ModArmorMaterials.EVANGELIST, ArmorItem.Type.BOOTS, new FabricItemSettings()));
+    ;;
 
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(Holyhell.MOD_ID, name), item);
     }
-    public static void registerModItems(){
+
+    public static void registerModItems() {
         Holyhell.LOGGER.info("registering items for" + Holyhell.MOD_ID);
 
 
