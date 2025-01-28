3fe32f47e9ee2a5c88da93d96edbbcd29db7b78d
The-Dead-Comedian <113227369+The-Dead-Comedian@users.noreply.github.com>
2025-01-28 12:06:09 +0200

    The mass visual revamp is done boys

--- src/main/java/com/dead_comedian/holyhell/block/DiviningTableBlock.java
+++ src/main/java/com/dead_comedian/holyhell/block/DiviningTableBlock.java
@@ -30,11 +30,7 @@ import java.util.List;
 public class DiviningTableBlock extends HorizontalFacingBlock {
 
 
-
-
     public static final List<BlockPos> POWER_PROVIDER_OFFSETS = BlockPos.stream(-2, 0, -2, 2, 1, 2).filter(pos -> Math.abs(pos.getX()) == 2 || Math.abs(pos.getZ()) == 2).map(BlockPos::toImmutable).toList();
-
-
 
 
     public DiviningTableBlock(AbstractBlock.Settings settings) {
@@ -49,15 +45,20 @@ public class DiviningTableBlock extends HorizontalFacingBlock {
     public static boolean canAccessPowerProvider(World world, BlockPos tablePos, BlockPos providerOffset) {
         return world.getBlockState(tablePos.add(providerOffset)).isOf(getBlockFromItem(Raid.getOminousBanner().getItem())) && world.getBlockState(tablePos.add(providerOffset.getX() / 2, providerOffset.getY(), providerOffset.getZ() / 2)).isIn(BlockTags.ENCHANTMENT_POWER_TRANSMITTER);
     }
+
     @Override
     public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
         super.randomDisplayTick(state, world, pos, random);
         for (BlockPos blockPos : POWER_PROVIDER_OFFSETS) {
-        
+
 
             if (random.nextInt(16) != 0 || !DiviningTableBlock.canAccessPowerProvider(world, pos, blockPos)) continue;
-            world.addParticle(ParticleTypes.ENCHANT, (double)pos.getX() + 0.5, (double)pos.getY() + 2.0, (double)pos.getZ() + 0.5, (double)((float)blockPos.getX() + random.nextFloat()) - 0.5, (float)blockPos.getY() - random.nextFloat() - 1.0f, (double)((float)blockPos.getZ() + random.nextFloat()) - 0.5);
-       System.out.println("HOyiaaa");
+            for (int i = 0; i <= 20; i++) {
+
+
+                world.addParticle(ParticleTypes.ENCHANT, (double) pos.getX() + 0.5, (double) pos.getY() + 2.0, (double) pos.getZ() + 0.5, (double) ((float) blockPos.getX() + random.nextFloat()) - 0.5, (float) blockPos.getY() - random.nextFloat() - 1.0f, (double) ((float) blockPos.getZ() + random.nextFloat()) - 0.5);
+            }
+
 
         }
     }
@@ -65,8 +66,8 @@ public class DiviningTableBlock extends HorizontalFacingBlock {
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-       player.addStatusEffect(new StatusEffectInstance(HolyHellEffects.ENLIGHTENED, 2400));
-       world.playSoundAtBlockCenter(pos, SoundEvents.ENTITY_ENDER_DRAGON_GROWL, SoundCategory.BLOCKS,1,1, false);
+        player.addStatusEffect(new StatusEffectInstance(HolyHellEffects.ENLIGHTENED, 2400));
+        world.playSoundAtBlockCenter(pos, SoundEvents.ENTITY_ENDER_DRAGON_GROWL, SoundCategory.BLOCKS, 1, 1, false);
         return ActionResult.SUCCESS;
     }
 
