624047732efca6d67d2bfac445fb35c11427e649
The-Dead-Comedian <113227369+The-Dead-Comedian@users.noreply.github.com>
2025-05-19 18:03:34 +0300

    Bunch of retextures

--- src/main/java/com/dead_comedian/holyhell/client/model/entity/HereticModel.java
+++ src/main/java/com/dead_comedian/holyhell/client/model/entity/HereticModel.java
@@ -30,28 +30,26 @@ public class HereticModel<T extends HereticEntity> extends HierarchicalModel<T> 
 
 		PartDefinition hole = partdefinition.addOrReplaceChild("hole", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-		PartDefinition bottom = hole.addOrReplaceChild("bottom", CubeListBuilder.create().texOffs(0, 20).addBox(-4.5F, -5.1174F, -13.4803F, 9.0F, 9.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -12.5F, 14.0F));
+		PartDefinition bottom = hole.addOrReplaceChild("bottom", CubeListBuilder.create().texOffs(0, 39).addBox(-4.5F, -5.1174F, -19.4802F, 9.0F, 9.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -12.5F, 14.0F));
 
-		PartDefinition back_right_leg2 = bottom.addOrReplaceChild("back_right_leg2", CubeListBuilder.create().texOffs(20, 47).addBox(-2.5F, 0.0F, -2.5F, 5.0F, 9.0F, 5.0F, new CubeDeformation(0.0F))
-		.texOffs(40, 0).addBox(-2.5F, 9.0F, -2.5F, 5.0F, 6.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(-5.0F, -2.5F, -4.5F));
+		PartDefinition back_right_leg2 = bottom.addOrReplaceChild("back_right_leg2", CubeListBuilder.create().texOffs(0, 20).addBox(-2.5F, 0.0F, -2.5F, 5.0F, 9.0F, 5.0F, new CubeDeformation(0.0F))
+				.texOffs(40, 0).addBox(-2.5F, 9.0F, -2.5F, 5.0F, 6.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(-5.0F, -2.5F, -4.5F));
 
 		PartDefinition back_left_leg2 = bottom.addOrReplaceChild("back_left_leg2", CubeListBuilder.create().texOffs(40, 0).addBox(-2.5F, 9.0F, -2.5F, 5.0F, 6.0F, 5.0F, new CubeDeformation(0.0F))
-		.texOffs(20, 47).addBox(-2.5F, 0.0F, -2.5F, 5.0F, 9.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(5.0F, -2.5F, -4.5F));
+				.texOffs(0, 20).addBox(-2.5F, 0.0F, -2.5F, 5.0F, 9.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(5.0F, -2.5F, -4.5F));
 
-		PartDefinition body = bottom.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-5.5F, -5.1174F, -9.0F, 11.0F, 11.0F, 9.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -1.0F, -13.4803F));
+		PartDefinition body = bottom.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-5.5F, -7.6174F, -7.0F, 11.0F, 11.0F, 9.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -1.0F, -13.4802F));
 
-		PartDefinition head = body.addOrReplaceChild("head", CubeListBuilder.create().texOffs(30, 31).addBox(-4.0F, -4.0F, -8.5F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.5F))
-		.texOffs(32, 12).addBox(-4.0F, -4.0F, -8.5F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.4796F, -8.952F));
+		PartDefinition head = body.addOrReplaceChild("head", CubeListBuilder.create().texOffs(32, 12).addBox(-4.0F, -3.0F, -8.5F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.5F)), PartPose.offset(0.0F, -3.5204F, -6.952F));
 
-		PartDefinition front_right_leg = body.addOrReplaceChild("front_right_leg", CubeListBuilder.create().texOffs(20, 47).addBox(-2.5F, 0.0F, -2.5F, 5.0F, 9.0F, 5.0F, new CubeDeformation(0.0F))
-		.texOffs(40, 47).addBox(-2.5F, 9.0F, -2.5F, 5.0F, 8.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(-6.0F, -3.5F, -5.5197F));
+		PartDefinition front_right_leg = body.addOrReplaceChild("front_right_leg", CubeListBuilder.create().texOffs(0, 20).addBox(-2.5F, 0.0F, -2.5F, 5.0F, 9.0F, 5.0F, new CubeDeformation(0.0F))
+				.texOffs(44, 28).addBox(-2.5F, 9.0F, -2.5F, 5.0F, 8.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(-6.0F, -3.5F, -5.5198F));
 
-		PartDefinition front_left_leg = body.addOrReplaceChild("front_left_leg", CubeListBuilder.create().texOffs(40, 47).addBox(-2.4938F, 9.0959F, -2.4465F, 5.0F, 8.0F, 5.0F, new CubeDeformation(0.0F))
-		.texOffs(20, 47).addBox(-2.4938F, 0.0959F, -2.4465F, 5.0F, 9.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(5.9938F, -3.5959F, -5.5732F));
+		PartDefinition front_left_leg = body.addOrReplaceChild("front_left_leg", CubeListBuilder.create().texOffs(44, 28).addBox(-2.4938F, 9.0959F, -2.4465F, 5.0F, 8.0F, 5.0F, new CubeDeformation(0.0F))
+				.texOffs(0, 20).addBox(-2.4938F, 0.0959F, -2.4465F, 5.0F, 9.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(5.9938F, -3.5959F, -5.5732F));
 
 		return LayerDefinition.create(meshdefinition, 64, 64);
 	}
-
 	@Override
 	public void setupAnim(HereticEntity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 		this.root().getAllParts().forEach(ModelPart::resetPose);
