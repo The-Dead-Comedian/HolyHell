c9b09ab2797adf0d07069f1aa7ac339f9edcec04
The-Dead-Comedian <113227369+The-Dead-Comedian@users.noreply.github.com>
2025-08-23 22:08:17 +0200

    Katar bug fixed

--- src/main/java/com/dead_comedian/holyhell/effect/BloodlustEffect.java
+++ src/main/java/com/dead_comedian/holyhell/effect/BloodlustEffect.java
@@ -5,12 +5,14 @@ import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 
 public class BloodlustEffect extends MobEffect {
     protected final RandomSource random = RandomSource.create();
     public boolean woah = false;
+    AttributeInstance health;
 
     public BloodlustEffect(MobEffectCategory statusEffectCategory, int color) {
         super(statusEffectCategory, color);
@@ -19,6 +21,7 @@ public class BloodlustEffect extends MobEffect {
 
     @Override
     public void onEffectAdded(LivingEntity livingEntity, int amplifier) {
+        health = livingEntity.getAttribute(Attributes.MAX_HEALTH);
         if (!woah) {
             livingEntity.getAttribute(Attributes.MAX_HEALTH).setBaseValue(livingEntity.getAttributeBaseValue(Attributes.MAX_HEALTH) * 0.8);
             livingEntity.setHealth((float) (livingEntity.getHealth() * 0.8));
@@ -31,7 +34,7 @@ public class BloodlustEffect extends MobEffect {
     public void onMobRemoved(LivingEntity livingEntity, int amplifier, Entity.RemovalReason reason) {
         super.onMobRemoved(livingEntity, amplifier, reason);
         if (woah) {
-            livingEntity.getAttribute(Attributes.MAX_HEALTH).setBaseValue(livingEntity.getAttributeBaseValue(Attributes.MAX_HEALTH) / 0.8);
+            livingEntity.getAttribute(Attributes.MAX_HEALTH).setBaseValue(health.getValue());
             woah = false;
         }
     }
--- src/main/java/com/dead_comedian/holyhell/event/HolyHellEventBusEvents.java
+++ src/main/java/com/dead_comedian/holyhell/event/HolyHellEventBusEvents.java
@@ -1,20 +1,28 @@
 package com.dead_comedian.holyhell.event;
 
 
-
 import com.dead_comedian.holyhell.Holyhell;
 import com.dead_comedian.holyhell.entity.*;
 import com.dead_comedian.holyhell.registries.HolyHellBlocks;
 import com.dead_comedian.holyhell.registries.HolyHellCriteriaTriggers;
 import com.dead_comedian.holyhell.registries.HolyHellEffects;
 import com.dead_comedian.holyhell.registries.HolyHellEntities;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.Cow;
+import net.minecraft.world.entity.animal.FlyingAnimal;
 import net.minecraft.world.entity.item.FallingBlockEntity;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.monster.RangedAttackMob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -42,7 +50,7 @@ public class HolyHellEventBusEvents {
     @SubscribeEvent
     public static void onLivingHealEvent(EntityJoinLevelEvent event) {
         if (event.getEntity() instanceof Cow cow) {
-            if(isAprilFools()){
+            if (isAprilFools()) {
                 cow.discard();
                 BlockPos blockPos = cow.blockPosition();
                 HolyCowEntity holyCowEntity = new HolyCowEntity(HolyHellEntities.HOLY_COW.get(), cow.level());
@@ -51,6 +59,7 @@ public class HolyHellEventBusEvents {
             }
         }
     }
+
     private static boolean isAprilFools() {
         LocalDate localdate = LocalDate.now();
         int i = localdate.get(ChronoField.DAY_OF_MONTH);
@@ -79,16 +88,57 @@ public class HolyHellEventBusEvents {
         }
     }
 
-   @SubscribeEvent
-   public static void registerAttributes(EntityAttributeCreationEvent event) {
-       event.put(HolyHellEntities.ANGEL.get(), AngelEntity.createAttributes().build());
-       event.put(HolyHellEntities.BAB_ONE.get(), BabOneEntity.createAttributes().build());
-       event.put(HolyHellEntities.BAB_TWO.get(), BabTwoEntity.createAttributes().build());
-       event.put(HolyHellEntities.BAB_THREE.get(), BabThreeEntity.createAttributes().build());
-       event.put(HolyHellEntities.CHERUB.get(), CherubEntity.createAttributes().build());
-       event.put(HolyHellEntities.HERETIC.get(), HereticEntity.createAttributes().build());
-       event.put(HolyHellEntities.HOLY_COW.get(), HolyCowEntity.createAttributes().build());
-       event.put(HolyHellEntities.HOLY_SPIRIT.get(), HolySpiritEntity.createAttributes().build());
-       event.put(HolyHellEntities.KAMIKAZE.get(), KamikazeEntity.createAttributes().build());
-   }
+    @SubscribeEvent
+    public static void livingEntityDie(LivingDeathEvent event) {
+
+
+        LivingEntity entity = event.getEntity();
+        LivingEntity entity1 = (LivingEntity) event.getSource().getEntity();
+        System.out.println("entity: " + entity);
+        System.out.println("entity1: " + entity1);
+
+
+        if(entity== Minecraft.getInstance().player && entity.hasEffect(HolyHellEffects.BLOODLUST)){
+            entity.setHealth(20);
+        }
+
+
+        if (entity instanceof LivingEntity && entity1 != null && entity!=entity1) {
+            if (entity1.hasEffect(HolyHellEffects.BLOODLUST)) {
+                entity1.setHealth((float) (entity1.getHealth() + entity.getAttribute(Attributes.MAX_HEALTH).getBaseValue() * 0.3F));
+
+
+                if (entity instanceof RangedAttackMob) {
+                    entity1.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 1));
+
+                }
+                if (entity instanceof FlyingAnimal) {
+                    entity1.addEffect(new MobEffectInstance(MobEffects.JUMP, 200, 1));
+
+                }
+                if (entity instanceof Monster) {
+                    entity1.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 200, 1));
+
+                }
+                if (entity instanceof Animal) {
+                    entity1.addEffect(new MobEffectInstance(MobEffects.SATURATION, 200, 1));
+
+                }
+            }
+        }
+
+    }
+
+    @SubscribeEvent
+    public static void registerAttributes(EntityAttributeCreationEvent event) {
+        event.put(HolyHellEntities.ANGEL.get(), AngelEntity.createAttributes().build());
+        event.put(HolyHellEntities.BAB_ONE.get(), BabOneEntity.createAttributes().build());
+        event.put(HolyHellEntities.BAB_TWO.get(), BabTwoEntity.createAttributes().build());
+        event.put(HolyHellEntities.BAB_THREE.get(), BabThreeEntity.createAttributes().build());
+        event.put(HolyHellEntities.CHERUB.get(), CherubEntity.createAttributes().build());
+        event.put(HolyHellEntities.HERETIC.get(), HereticEntity.createAttributes().build());
+        event.put(HolyHellEntities.HOLY_COW.get(), HolyCowEntity.createAttributes().build());
+        event.put(HolyHellEntities.HOLY_SPIRIT.get(), HolySpiritEntity.createAttributes().build());
+        event.put(HolyHellEntities.KAMIKAZE.get(), KamikazeEntity.createAttributes().build());
+    }
 }
